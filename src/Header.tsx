f3e81d7d7cba0b69c4b4df56bcf32e7a81ac49c6
mklewandowski <matt.kain.lewandowski@gmail.com>
2021-06-01 09:42:04 -0700

    Fix font, replace with SVG

--- src/Header.tsx
+++ src/Header.tsx
@@ -9,7 +9,14 @@ export const Header: React.FC = () => {
       CONWAY COLOR LIFE
       <div className="description">
         Conway Color Life is an example of a cellular automaton, a collection of cells that change over time based on a set of simple rules. Click cells to turn them alive, and then press the Start button to see what happens.
-        {!expanded && <div className="expand" onClick={() => setExpanded(true)}>Learn More<i className="fas fa-angle-down icon" /></div>}
+        {!expanded &&
+          <div className="expand" onClick={() => setExpanded(true)}>
+            Learn More
+            <svg height="12" width="16" className="icon">
+              <polygon points="8,12 0,0 16,0" style={{fill:"aliceblue"}} />
+            </svg>
+          </div>
+        }
         {expanded &&
           <div className="more-content">
             <div>Cells are either alive or dead. At each generation a cell is marked as alive or dead based on the following rules:</div>
@@ -20,7 +27,12 @@ export const Header: React.FC = () => {
               <li>A dead cell with 3 living neighbors becomes alive</li>
             </ul>
             <div>With each generation that a cell survives, the cell ages. Cells change color as they age based on the selected color theme.</div>
-            <div className="expand" onClick={() => setExpanded(false)}>Less<i className="fas fa-angle-up icon" /></div>
+            <div className="expand" onClick={() => setExpanded(false)}>
+              Less
+              <svg height="12" width="16" className="icon">
+                <polygon points="8,0 16,12 0,12" style={{fill:"aliceblue"}} />
+              </svg>
+            </div>
           </div>
         }
       </div>
