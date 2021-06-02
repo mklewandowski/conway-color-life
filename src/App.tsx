612c496844778654f0166ebaf7e7de5fe63cac91
mklewandowski <matt.kain.lewandowski@gmail.com>
2021-06-01 22:31:27 -0700

    Add more colors and shapes

--- src/App.tsx
+++ src/App.tsx
@@ -33,6 +33,10 @@ const colorThemes: ColorTheme[] = [
   colors: ["#00b5e2", "#19adee", "#4ba2f5", "#7795f4", "#a084ea", "#c270d6", "#dd59b9", "#ef4294", "#f5326b", "#ef3340",
     "#f5326b", "#ef4294", "#dd59b9", "#c270d6", "#a084ea", "#7795f4", "#4ba2f5", "#19adee", "#00b5e2"]
   },
+  { name: "Indigo-Fuchsia",
+    colors: ["#7200fe", "#8a00fb", "#9e00f8", "#b000f4", "#c000f1", "#cf00ee", "#dc00ea", "#e900e7", "#f400e4", "#ff00e1",
+      "#f400e4", "#e900e7", "#dc00ea", "#cf00ee", "#c000f1", "#b000f4", "#9e00f8", "#8a00fb", "#7200fe"]
+  },
   { name: "Magenta-Lime",
     colors: ["#a31746", "#b62a40", "#c73f39", "#d35530", "#dd6c24", "#e28315", "#e39b00", "#dfb400", "#d8cc02", "#cbe426",
       "#d8cc02", "#dfb400", "#e39b00", "#e28315", "#dd6c24", "#d35530", "#c73f39", "#b62a40", "#a31746"]
@@ -41,10 +45,10 @@ const colorThemes: ColorTheme[] = [
     colors: ["#00ff00", "#6fed00", "#98db00", "#b6c700", "#cdb200", "#df9b00", "#ee8200", "#f86600", "#fe4400", "#ff0000",
       "#fe4400", "#f86600", "#ee8200", "#df9b00", "#cdb200", "#b6c700", "#98db00", "#6fed00", "#00ff00"]
   },
-  { name: "Rainbow",
+  { name: "Confetti",
     colors: ["#ff0000", "#ffa500", "#ffff00", "#008000", "#0000ff", "#4b0082", "#ee82ee", "#4b0082", "#0000ff", "#008000", "#ffff00", "#ffa500", "#ff0000"]
   },
-  { name: "Pinks",
+  { name: "Valentine",
     colors: ["#fa6e6e", "#ff6374", "#ff587c", "#ff4b86", "#ff3d92", "#ff2da0", "#ff1ab0", "#ff02c1", "#ff00d4", "#ff00e8",
       "#ff00d4", "#ff02c1", "#ff1ab0", "#ff2da0", "#ff3d92", "#ff4b86", "#ff587c", "#ff6374", "#fa6e6e"]
   },
@@ -59,6 +63,7 @@ const shapes: Shape[] = [
   { name: "Snake" },
   { name: "Bucket" },
   { name: "Double Bucket" },
+  { name: "Hexa-Bucket" },
   { name: "Glider" },
   { name: "Dragons" },
   { name: "R-Pentomino" },
@@ -163,6 +168,7 @@ function App() {
       newGridCells[middle - 1 - rows * 2 - 2] = {state: 1, age: 0};
       newGridCells[middle + 1 - rows - 2] = {state: 1, age: 0};
       newGridCells[middle + 1 - rows * 2 - 2] = {state: 1, age: 0};
+
       newGridCells[middle - 1 + 3] = {state: 1, age: 0};
       newGridCells[middle + 3] = {state: 1, age: 0};
       newGridCells[middle + 1 + 3] = {state: 1, age: 0};
@@ -170,6 +176,54 @@ function App() {
       newGridCells[middle - 1 - rows * 2 + 3] = {state: 1, age: 0};
       newGridCells[middle + 1 - rows + 3] = {state: 1, age: 0};
       newGridCells[middle + 1 - rows * 2 + 3] = {state: 1, age: 0};
+    } else if (shape === "Hexa-Bucket") {
+      newGridCells[middle - 1 - 2] = {state: 1, age: 0};
+      newGridCells[middle - 2] = {state: 1, age: 0};
+      newGridCells[middle + 1 - 2] = {state: 1, age: 0};
+      newGridCells[middle - 1 - rows - 2] = {state: 1, age: 0};
+      newGridCells[middle - 1 - rows * 2 - 2] = {state: 1, age: 0};
+      newGridCells[middle + 1 - rows - 2] = {state: 1, age: 0};
+      newGridCells[middle + 1 - rows * 2 - 2] = {state: 1, age: 0};
+
+      newGridCells[middle - 1 - 8] = {state: 1, age: 0};
+      newGridCells[middle - 8] = {state: 1, age: 0};
+      newGridCells[middle + 1 - 8] = {state: 1, age: 0};
+      newGridCells[middle - 1 - rows - 8] = {state: 1, age: 0};
+      newGridCells[middle - 1 - rows * 2 - 8] = {state: 1, age: 0};
+      newGridCells[middle + 1 - rows - 8] = {state: 1, age: 0};
+      newGridCells[middle + 1 - rows * 2 - 8] = {state: 1, age: 0};
+
+      newGridCells[middle - 1 - 13] = {state: 1, age: 0};
+      newGridCells[middle - 13] = {state: 1, age: 0};
+      newGridCells[middle + 1 - 13] = {state: 1, age: 0};
+      newGridCells[middle - 1 - rows - 13] = {state: 1, age: 0};
+      newGridCells[middle - 1 - rows * 2 - 13] = {state: 1, age: 0};
+      newGridCells[middle + 1 - rows - 13] = {state: 1, age: 0};
+      newGridCells[middle + 1 - rows * 2 - 13] = {state: 1, age: 0};
+
+      newGridCells[middle - 1 + 3] = {state: 1, age: 0};
+      newGridCells[middle + 3] = {state: 1, age: 0};
+      newGridCells[middle + 1 + 3] = {state: 1, age: 0};
+      newGridCells[middle - 1 - rows + 3] = {state: 1, age: 0};
+      newGridCells[middle - 1 - rows * 2 + 3] = {state: 1, age: 0};
+      newGridCells[middle + 1 - rows + 3] = {state: 1, age: 0};
+      newGridCells[middle + 1 - rows * 2 + 3] = {state: 1, age: 0};
+
+      newGridCells[middle - 1 + 9] = {state: 1, age: 0};
+      newGridCells[middle + 9] = {state: 1, age: 0};
+      newGridCells[middle + 1 + 9] = {state: 1, age: 0};
+      newGridCells[middle - 1 - rows + 9] = {state: 1, age: 0};
+      newGridCells[middle - 1 - rows * 2 + 9] = {state: 1, age: 0};
+      newGridCells[middle + 1 - rows + 9] = {state: 1, age: 0};
+      newGridCells[middle + 1 - rows * 2 + 9] = {state: 1, age: 0};
+
+      newGridCells[middle - 1 + 14] = {state: 1, age: 0};
+      newGridCells[middle + 14] = {state: 1, age: 0};
+      newGridCells[middle + 1 + 14] = {state: 1, age: 0};
+      newGridCells[middle - 1 - rows + 14] = {state: 1, age: 0};
+      newGridCells[middle - 1 - rows * 2 + 14] = {state: 1, age: 0};
+      newGridCells[middle + 1 - rows + 14] = {state: 1, age: 0};
+      newGridCells[middle + 1 - rows * 2 + 14] = {state: 1, age: 0};
     } else if (shape === "Dragons") {
       newGridCells[middle - 2] = {state: 1, age: 0};
       newGridCells[middle - 3] = {state: 1, age: 0};
